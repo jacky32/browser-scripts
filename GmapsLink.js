f1ce6a1393be4186fb31d3d97f166fec98bb18f5
jacky32 <4532511+jacky32@users.noreply.github.com>
2024-06-30 14:23:01 +0200

    Create GmapsLink.js

---
+++ GmapsLink.js
@@ -0,0 +1,90 @@
+// ==UserScript==
+// @name     GmapsLink
+// @version  1
+// @grant    none
+// @include https://www.google.com/search*
+// ==/UserScript==
+var observeDOM = (function(){
+  var MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+
+  return function( obj, callback ){
+    if( !obj || obj.nodeType !== 1 ) return; 
+
+    if( MutationObserver ){
+      // define a new observer
+      var mutationObserver = new MutationObserver(callback)
+
+      // have the observer observe for changes in children
+      mutationObserver.observe( obj, { childList:true, subtree:true })
+      return mutationObserver
+    }
+    
+    // browser support fallback
+    else if( window.addEventListener ){
+      obj.addEventListener('DOMNodeInserted', callback, false)
+      obj.addEventListener('DOMNodeRemoved', callback, false)
+    }
+  }
+})()
+
+let isMapOnPage = false;
+
+observeDOM(document.querySelector('html'), function(m){ 
+  if (!isMapOnPage) {    
+    // const mapsElements = Array.from(document.querySelectorAll('script'));
+    //const includes = mapsElements.some(mapsElement => mapsElement.src.includes('https://maps.googleapis.com/')));
+    const includes = m.some(a => a.target.outerHTML.includes('mapa') );
+    if (includes) {
+      const params = new URLSearchParams(window.location.search);
+      console.log(`https://www.google.com/maps/search/?api=1&query=${params.get('q')}`);
+      const forms = document.querySelectorAll('form');
+      forms.forEach(form => {
+        if (form.action == 'https://www.google.com/search') {
+          const aNode = document.createElement("a");
+          const text = document.createTextNode("Mapy");
+          aNode.style.zIndex = '500';
+          aNode.style.margin = 'auto';
+          aNode.style.background = '#303134';
+          aNode.style.borderRadius = '30px';
+          aNode.style.padding= '14.5px';
+          aNode.style.color = 'white';
+          aNode.style.textDecoration = 'none';
+          aNode.style.marginLeft = '32px';
+          aNode.style.fontWeight = 'bolder';
+          aNode.href = `https://www.google.com/maps/search/?api=1&query=${params.get('q')}`
+          aNode.appendChild(text);
+          form.parentNode.insertBefore(aNode, form.nextSibling);
+          isMapOnPage = true;
+        }
+      });
+    } 
+  }
+           
+// window.addEventListener('load', function() {
+//  setTimeout(()=> {
+//    const mapsElements = Array.from(document.querySelectorAll('script'));
+//    const includes = mapsElements.some(mapsElement => {
+//      if (mapsElement.src.includes('https://maps.googleapis.com/')) {
+//        return true;
+//      } else {
+//        return false;
+//      }
+//    });
+//    if (includes) {
+//      const params = new URLSearchParams(window.location.search);
+//      console.log(`https://www.google.com/maps/search/?api=1&query=${params.get('q')}`);
+//      const forms = document.querySelectorAll('form');
+//      forms.forEach(form => {
+//        if (form.action == 'https://www.google.com/search') {
+//          const aNode = document.createElement("a");
+//					const text = document.createTextNode("Mapy");
+//          aNode.style.zIndex = '500';
+//          aNode.backgroundColor = 'blue';
+//          aNode.href = `https://www.google.com/maps/search/?api=1&query=${params.get('q')}`
+//          aNode.appendChild(text);
+//          form.parentNode.insertBefore(aNode, form.nextSibling);
+//        }
+//      });
+//    } 
+//  }, 3000);
+//}, false);
